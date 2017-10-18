f8e2401f447ae6cf48ed660de6674116b4688369
Ilija Eftimov <ileeftimov@gmail.com>
2017-10-18 15:25:18 +0200

    Add Bid#amount_larger_than_previous_bid validation

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,6 +1,15 @@
 class Bid < ApplicationRecord
   belongs_to :lot
-  belongs_to :user
 
   validates :amount, presence: true, numericality: true
+  validate :amount_larger_than_previous_bid
+
+  private
+
+  def amount_larger_than_previous_bid
+    return true if lot.bids.none?
+    return true if amount > lot.bids.last.amount
+
+    errors.add(:amount, 'has to be larger then previous bid')
+  end
 end
